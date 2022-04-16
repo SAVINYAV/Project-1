dfa8683c2132e4118f732026159ed7dcefb2a516
SAVINYAV <103824562+SAVINYAV@users.noreply.github.com>
2022-04-16 16:02:06 +0500

    Update Тест Руфье.py

--- Тест Руфье.py
+++ Тест Руфье.py
@@ -1,1 +1,44 @@
-print('Hello')
+from PyQt5.QtWidgets import QLabel, QWidget, QApplication,QVBoxLayout,QHBoxLayout,QPushButton,QLineEdit
+from PyQt5.QtCore import Qt
+
+class MainWin(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set_appear()
+        self.unitUI()
+        self.connects()
+        self.show()
+
+
+    def set_appear(self):
+        self.resize(600,400)
+        self.setWindowTitle("Инструкция")
+        self.move(250,250)
+
+
+
+    def unitUI(self):
+        hello = QLabel("Здесь будет приветствие")
+        instr = QLabel("Здесь будет инсрукция")
+        button = QPushButton("Начать")
+        v_line = QVBoxLayout()
+
+    def connects(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main = MainWin()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    app.exec_()
